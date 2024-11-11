f40c5df2d18ca1995691424080b243e127d950f1
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-11-11 14:56:57 -0500

    11-nov

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SetLocaleController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,6 +42,9 @@ Route::middleware('auth')->group(function() {
 
 });
 
+
+Route::get('/create/category', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/create/category', [CategoryController::class, 'store'])->name('category.store');
 
 Route::get('/login', [AuthController::class, 'create'])->name('login');
 Route::post('/login', [AuthController::class, 'store'])->name('login.store');
